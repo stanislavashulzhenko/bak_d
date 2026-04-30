c1ce2e5f0301099197610e8ccf46bddc22a03fd1
stanislavasulzenko <144595053+stanislavashulzhenko@users.noreply.github.com>
2026-05-01 01:39:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -301,20 +301,24 @@ FEEDBACK:
 
 
 
+const PORT = process.env.PORT || 3000;
 
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+// app.listen(3000, () => {
+//     console.log("Server running on http://localhost:3000");
+// });
 
 
 // node server js
